5ea2bcf3cad716b1872789f85ce8b6eff52fcd17
Satyaprakash <satyachinkul38@gmail.com>
2023-09-14 02:12:21 +0530

    added all file

---
+++ js/music-list.js
@@ -0,0 +1,61 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+
+
+  //new songs
+  
+  {
+    name: "Aane Se Uske Aaye Bahar",
+    artist: "Mohammed Rafi",
+    img: "Aane Se Uske Aaye Bahar",
+    src: "Aane-Se-Uske-Aaye-Bahar"
+  },
+
+  
+  {
+    name: "Chahoonga Main Tujhe",
+    artist: "Mohammed Rafi & Laxmikant–Pyarelal",
+    img: "Chahoonga Main Tujhe",
+    src: "Chahoonga Main Tujhe"
+  },
+{
+    name: "Koi Hamdam Na Raha",
+    artist: "Kishore Kumar",
+    img: "Koi Hamdam Na Raha",
+    src: "Koi Hamdam Na Raha",
+},
+{
+    name: "Lakhon Hain Nigahon Mein",
+    artist: "Mohammed Rafi",
+    img: "Lakhon Hain Nigahon Mein",
+    src: "Lakhon Hain Nigahon Mein",
+},
+{
+    name: "Main Jat Yamla Pagla Diwana",
+    artist: "Mohammed Rafi & Laxmikant–Pyarelal",
+    img: "Main Jat Yamla Pagla Diwana",
+    src: "Main Jat Yamla Pagla Diwana",
+},
+{
+    name: "Mausam Hai Ashiqana",
+    artist: "Lata Mangeshkar",
+    img: "Mausam Hai Ashiqana",
+    src: "Mausam Hai Ashiqana",
+},
+{
+    name: "Pardesiyon Se Na Ankhiyan Milana - Happy",
+    artist: "Mohammed Rafi & Kalyanji-Anandji",
+    img: "Pardesiyon Se Na Ankhiyan Milana - Happy",
+    src: "Pardesiyon Se Na Ankhiyan Milana - Happy",
+}
+];
